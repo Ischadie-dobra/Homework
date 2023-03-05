2c6cd8938b912023af402e03b14750d137b8ec8e
Ischadie-dobra <125760331+Ischadie-dobra@users.noreply.github.com>
2023-03-05 11:56:02 +0300

    Домашнее задание 2

---
+++ Homework1/Homework2/Program.cs
@@ -0,0 +1,18 @@
+﻿//Task 10. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int Number(int num)
+{
+   int hundreds = num % 100;
+   int ones = hundreds / 10;
+   
+   return ones;
+ }
+   
+Console.Write("Введите трехзначное число: ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Number(num));
+
+
+
