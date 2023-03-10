29cb445f6b6660c96a491725fe6f8c7807d3f01a
Ischadie-dobra <125760331+Ischadie-dobra@users.noreply.github.com>
2023-03-10 20:21:39 +0300

    Homework3

--- Program.cs
+++ Program.cs
@@ -1,77 +1,69 @@
-﻿//Task 10. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+﻿//Задача 19
+//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+//14212 -> нет
+//12821 -> да
+//23432 -> да
 /*
-int Number(int num)
+Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+int count = number.Length;
+
+if (count == 5)
 {
-   int hundreds = num % 100;
-   int ones = hundreds / 10;
-   
-   return ones;
- }
-   
-Console.Write("Введите трехзначное число: ");
-
-int num = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine(Number(num));
-
-*/
-
-//Task 13. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-
-//645 -> 5
-//78 -> третьей цифры нет
-//32679 -> 6
-/*
-int ThirdNumber(int num)
-{
-    while(num > 999)
+    if (number[0] == number[4] && number[1] == number[3])
     {
-    num /= 10;
-    }
-    return num % 10;
-}
-
-bool ValidatNumber(int num)
-{
-    if(num < 100)
-    {
-        Console.WriteLine("Третьей цифры нет");
-        return false;
-    }
-    return true;
-}
-
-Console.Write("Input a number: ");
-int num = Convert.ToInt32(Console.ReadLine());
-if (ValidatNumber(num));
-{
-Console.WriteLine(ThirdNumber(num));
-}
-*/
-
-
-
-//Task 15. Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
-
-//6 -> да
-//7 -> да
-//1 -> нет
-/*
-void IsDayWeek(int number)
-{
-    if (number == 6 || number == 7)
-    {
-        Console.WriteLine("Да, это выходной");
+        Console.WriteLine($"{number} - Палиндром");
     }
     else
-    if (number > 1 || number < 7)
     {
-        Console.WriteLine("Нет, это не выходной");
+        Console.WriteLine($"{number} - НЕ палиндром");
     }
+}
+*/
 
+
+//Задача 21
+//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+//A (3,6,8); B (2,1,-7), -> 15.84
+//A (7,-5, 0); B (1,-1,9) -> 11.53
+
+/*
+double Distance3D(double x1, double y1, double z1, double x2, double y2, double z2)
+{
+    double res = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+    return Math.Round(res, 2);
 }
 
-Console.Write("Input a day number: ");
-int numb = Convert.ToInt32(Console.ReadLine());
-IsDayWeek(numb);
+Console.Write("Введите координату по Х для первой точки: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату по У для первой точки: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату по Z для первой точки: ");
+double z1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату по Х для второй точки: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату по Y для второй точки: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату по Z для второй точки: ");
+double z2 = Convert.ToDouble(Console.ReadLine());
+
+double distance = Distance3D(x1, y1, z1, x2, y2, z2);
+System.Console.WriteLine($"Расстояние между точками = {distance}");
+*/
+
+
+
+
+
+//Задача 23
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+/*
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+for(int i = 1; i <= number; i++)
+{
+Console.Write($"{Math.Pow(i, 3)} ");
+}
 */
